f680b14777c6ef8f511d70c16093cd65f07d2641
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-05 00:14:36 +0530

    total wage of month

--- empw.sh
+++ empw.sh
@@ -1,33 +1,51 @@
-#!/bin/bash 
+#!/bin/bash  
+
 echo "welcome to employee wage"
-empCheck=$((RANDOM%3))
+#empCheck=$((RANDOM%3))
 EMP_WAGE_PER_HR=20
 full_hr=8
 part_hr=4
+NUMBER_OF_WORKING_DAYS=20
+MAX_WORKING_HRS=100
+totalSalary=0
+totalEmpHr=0
+totalWorkingDays=0
+
+while [[ $totalEmpHr -le $MAX_WORKING_HRS || $totalWorkingDays -le $NUMBER_OF_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+
+
+empCheck=$((RANDOM%3))
 case $empCheck in
                 2)
-                        echo "Employee is Part-Time"
-                         wage=$(( $EMP_WAGE_PER_HR * $full_hr ))
-
-               	echo "wage per day "$wage 
+                        #echo "Employee is Part-Time"
+                 emphr=$part_hr         
+		wage=$(( $EMP_WAGE_PER_HR * $emphr ))
+               	#echo "wage per day "$wage 
                ;;
                 1)
-                        echo "Employee is Full-Time"
-                         
-                         wage=$(( $EMP_WAGE_PER_HR * $full_hr ))
+                        #echo "Employee is Full-Time"
+                         emphr=$full_hr
+                         wage=$(( $EMP_WAGE_PER_HR * $emphr ))
 
-                echo "wage per day "$wage
+              #  echo "wage per day "$wage
                 ;;
  		0)
-                        echo "Employee is absent"
+                        #echo "Employee is absent"
 
-                         wage=$(( $EMP_WAGE_PER_HR * $part_hr ))
+                  emphr=0       
+                  wage=$(( $EMP_WAGE_PER_HR * $emphr ))
 
-                echo "wage per day "$wage
+
+          #      echo "wage per day "$wage
 		;;
 
-                *)
-                        #echo "Employee is Absent"
-                        empHr=0
-                ;;
+              
+                                   
         esac
+totalEmpHr=$(( $totalEmpHr + $emphr ))
+totalSalary=$(( $totalEmpHr * $EMP_WAGE_PER_HR ))
+done
+
+echo "Total Wage of Emp:" $totalSalary
